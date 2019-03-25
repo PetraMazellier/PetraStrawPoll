50665242fd3f07ee07b9bac5065f7480ca7340a6
Petra MAZELLIER <coladaitp18-mpe@ccicampus.fr>
2019-03-25 16:08:13 +0100

    premier master

---
+++ StrawPoll/Controllers/PetrasStrawPollController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StrawPoll.Controllers
+{
+    public class PetrasStrawPollController : Controller
+    {
+        // GET: PetrasStrawPoll
+        public ActionResult Accueil()
+        {
+            return View();
+        }
+        public ActionResult Accueil1()
+        {
+            return View();
+        }
+        public ActionResult Creation()
+        {
+            return View();
+        }
+        public ActionResult Condition()
+        {
+            return View();
+        }
+        public ActionResult Vote()
+        {
+            return View();
+        }
+        public ActionResult ConfirmationCreation()
+        {
+            return View();
+        }
+        public ActionResult APropos()
+        {
+            return View();
+        }
+        public ActionResult Confidentialite()
+        {
+            return View();
+        }
+    }
+}
